fab9d4c05c1204825b2b57080dd9e5a35b6ddf47
joebobfisher <fisher.joe@gmail.com>
2017-04-22 18:22:24 -0500

    Docstrings, comments, and README, oh my!

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# ud036_StarterCode
+# entertainment_center
 Source code for a Movie Trailer website.
+
+## To Run
+Just run entertainment_center.py with python v2.7 or higher.
+
+```
+% py -2 entertainment_center.py
+```
+
+entertainment_center is not guaranteed to work with python v3.x.
--- entertainment-center.py
+++ entertainment-center.py
@@ -1,27 +1,21 @@
+"""entertainment-center.py is licensed under the same terms as python 2.7."""
+
+# Class from Udacity that acts as the front-end.
 import fresh_tomatoes
 
-# YOUR class (media.py)!
+# YOUR class (media.py)
 import media
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # NOQA
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
-
-#print(toy_story.storyline)
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",  # NOQA
                      "http://www.youtube.com/watch?v=-9ceBgWV8io")
-
-#print(avatar.storyline)
-
-#avatar.show_trailer()
 
 movies = [toy_story, avatar]
 
-#fresh_tomatoes.open_movies_page(movies)
-
-#print(media.Movie.VALID_RATINGS)
-print(media.Movie.__doc__)
+fresh_tomatoes.open_movies_page(movies)
