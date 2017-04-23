8e06550438f02a66555a4436a5ca17d24764f1b7
joebobfisher <fisher.joe@gmail.com>
2017-04-22 19:54:07 -0500

    Added ratings!
    Added ratings, and also docstrings and comments.

--- entertainment-center.py
+++ entertainment-center.py
@@ -9,13 +9,23 @@ import media
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # NOQA
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+                        "http://www.youtube.com/watch?v=vwyZH85NQC4",
+                        "G")
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",  # NOQA
-                     "http://www.youtube.com/watch?v=-9ceBgWV8io")
+                     "http://www.youtube.com/watch?v=-9ceBgWV8io",
+                     "PG-13")
 
-movies = [toy_story, avatar]
+rogue_one = media.Movie("Rogue One: A Star Wars Story",
+                        "The Rebel Alliance makes a risky move to steal the " +
+                        "plans for the Death Star, setting up the epic saga " +
+                        "to follow.",
+                        "http://upload.wikimedia.org/wikipedia/en/d/d4/Rogue_One%2C_A_Star_Wars_Story_poster.png",  # NOQA
+                        "http://www.youtube.com/watch?v=DLzxrzFCyOs",
+                        "PG-13")
+
+movies = [toy_story, avatar, rogue_one]
 
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,15 +1,53 @@
+"""media.py is licensed under the same terms as python 2.7."""
+
+# needed for Movie.show_trailer()
 import webbrowser
 
 class Movie():
-    """This class provides a way to store movie-related information"""
+    """Movie provides a way to store movie-related information.
+    
+    Attributes:
+        VALID_RATINGS: a dict of allowed ratings strings for movies
+            (PG, R, etc.), each which points to an image for that rating.
+    """
 
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    # Allowed ratings that we might need to use later.
+    VALID_RATINGS = {"G": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/RATED_G.svg/1200px-RATED_G.svg.png",  # NOQA
+                     "PG": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/RATED_PG.svg/1200px-RATED_PG.svg.png",  # NOQA
+                     "PG-13": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/RATED_PG-13.svg/1200px-RATED_PG-13.svg.png",  # NOQA
+                     "R": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/RATED_R.svg/1200px-RATED_R.svg.png"}  # NOQA
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube_url):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube_url
+    def __init__(self,
+                 movie_title,
+                 movie_storyline,
+                 poster_image,
+                 trailer_youtube_url,
+                 rating):
+        """Creates a Movie object.
+
+        Args:
+            movie_title: A string containing the title of the movie.
+            movie_storyline: A string containing a one-sentence summary or
+                tagline.
+            poster_image: A link to an image file that can be used for the
+                movie's poster.
+            trailer_youtube_url: A link to the movie's trailer on youtube.
+            rating: A string containing the movie's MPAA rating.
+        """
+        self.title                  = movie_title
+        self.storyline              = movie_storyline
+        self.poster_image_url       = poster_image
+        self.trailer_youtube_url    = trailer_youtube_url
+        self.rating                 = rating
+        
+        # If the user passed us an invalid rating, default to "R".
+        # Yes, this is harsh, but better than defaulting to "G"...
+        try:
+            self.rating_image_url = Movie.VALID_RATINGS[rating]
+        except KeyError:
+            self.rating           = "R"
+            self.rating_image_url = Movie.VALID_RATINGS["R"]
 
     def show_trailer(self):
+        """Shows the trailer listed in the Movie object."""
         webbrowser.open(self.trailer_youtube_url)
